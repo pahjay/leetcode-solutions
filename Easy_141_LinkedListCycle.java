36757c575affdaa9ebfbfaa51a25542f04a010ea
Ryan Page <rpage101@gmail.com>
2017-06-14 11:03:42 -0700

    added solution Easy_141_LinkedListCycle

---
+++ Easy_141_LinkedListCycle.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+/**
+ * if the linked list has a cycle the two pointers will meet at some point.
+ */
+public class Easy_141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+       if (head == null) return false;
+       ListNode tortoise = head;
+       ListNode hare = head;
+
+       while (tortoise.next != null && hare.next.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if (tortoise == hare) return true;
+        }
+        return false;
+    }
+}
